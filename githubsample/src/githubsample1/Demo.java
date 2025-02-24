da250a00f06038fa122fdef58cb8b68a57643687
SOWMYA Y T <SOWMYA Y T@LAPTOP-FRR416C2>
2025-02-24 22:23:38 +0530

    gitsample

---
+++ githubsample/src/githubsample1/Demo.java
@@ -0,0 +1,13 @@
+package githubsample1;
+
+public class Demo {
+	public static void main(String[] args) {
+		int a=9;
+		if(a%2==0) {
+			System.out.println("Number is even");
+		}
+		else {
+			System.out.println("Given number is odd");
+		}
+	}
+}
