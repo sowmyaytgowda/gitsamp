c9a9d8ca941370461b73c95aed4ab2df22de21af
SOWMYA Y T <SOWMYA Y T@LAPTOP-FRR416C2>
2025-02-25 18:29:48 +0530

    pushing demo2

---
+++ githubsample/src/githubsample1/Demo2.java
@@ -0,0 +1,15 @@
+package githubsample1;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		int a=9;
+		if(a%2==0) {
+			System.out.println("Number is even");
+		}
+		else {
+			System.out.println("Given number is odd");
+						System.out.println("Given number is odd");
+						System.out.println("Adding demo2 to github,pushing to github");
+
+		}
+}
